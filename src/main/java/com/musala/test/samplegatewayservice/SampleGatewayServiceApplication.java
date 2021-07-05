83d5c70594f132abcf533776e07d2cfbac24484a
Imtiaze A <imtiaze_a@yahoo.com>
2021-07-05 21:07:25 +0600

    Project Created

---
+++ src/main/java/com/musala/test/samplegatewayservice/SampleGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musala.test.samplegatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musala/test/samplegatewayservice/SampleGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.test.samplegatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
